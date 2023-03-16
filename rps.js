c67a5c68a3452840e991d81b2e2f2ae5937c1d18
hergottwilliam <hergottwilliam@gmail.com>
2023-03-16 19:00:39 -0400

    buttons now work for rock paper scissors, winner is displayed, but not result is printed as well as no game limit

--- rps.js
+++ rps.js
@@ -25,12 +25,31 @@ function playRound(p1_choice, p2_choice){
         return "p2";
     }
 }
+// scores in HTML
+const playerscore = document.querySelector('#playerscore');
+const cpuscore = document.querySelector('#cpuscore');
+
+const result = document.querySelector("#result");
+
+// scores in js
+let playerscore_js = 0;
+let cpuscore_js = 0;
 
 const buttons = document.querySelectorAll('.weapon');
 
 buttons.forEach((button) => {
     
     button.addEventListener('click', () =>{
-        console.log(playRound(button.id, getComputerChoice()));
+        if (playRound(button.id, getComputerChoice()) == 'p1'){
+            playerscore_js += 1;
+            playerscore.textContent = `You: ${playerscore_js}`;
+        }
+        else if (playRound(button.id, getComputerChoice()) == 'p2'){
+            cpuscore_js += 1;
+            cpuscore.textContent = `CPU: ${cpuscore_js}`;
+        }
+        else{
+            result.textContent = `Tie, ${button.id} ties $`;
+        }
     });
 });
