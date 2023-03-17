743eb18913f3d2004e40e81be12a811a335d3c42
hergottwilliam <hergottwilliam@gmail.com>
2023-03-17 07:59:30 -0400

    game finished, small visual changes made, game limit of 5 functions

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # rock-paper-scissors
-A simple rock-paper-scissors game against the CPU, with an even simplier UI. first to 5 points wins!
+A game of rock paper scissors against the cpu (really against chance). First to 5 points wins the game!
--- rps.js
+++ rps.js
@@ -1,3 +1,5 @@
+let gameStatus = true;
+
 let weapons = ["rock", "paper", "scissors"];
 
 let pairs = {
@@ -25,11 +27,26 @@ function playRound(p1_choice, p2_choice){
         return "p2";
     }
 }
+
+function checkWinner(p1_score, p2_score){
+    // p1 will be player, p2 will be CPU
+    if (p1_score == 5){
+        result.textContent = "GAME OVER, you win!";
+        gameStatus = false;
+    }
+    else if (p2_score == 5){
+        result.textContent = "GAME OVER, CPU wins!";
+        gameStatus = false;
+    }
+
+}
+
 // scores in HTML
 const playerscore = document.querySelector('#playerscore');
 const cpuscore = document.querySelector('#cpuscore');
 
 const result = document.querySelector("#result");
+result.textContent = "First to 5 points wins!"
 
 // scores in js
 let playerscore_js = 0;
@@ -37,23 +54,28 @@ let cpuscore_js = 0;
 
 const buttons = document.querySelectorAll('.weapon');
 
-// main loop
+// check for events (main loop)
 buttons.forEach((button) => {
     
     button.addEventListener('click', () =>{
         let tmp_cpu = getComputerChoice();
-        if (playRound(button.id, tmp_cpu) == 'p1'){
-            playerscore_js += 1;
-            playerscore.textContent = `You: ${playerscore_js}`;
-            result.textContent = `You win, ${button.id} beats ${tmp_cpu}`;
+        if (gameStatus == true){
+            if (playRound(button.id, tmp_cpu) == 'p1'){
+                playerscore_js += 1;
+                playerscore.textContent = `You: ${playerscore_js}`;
+                result.textContent = `You win, ${button.id} beats ${tmp_cpu}`;
+                checkWinner(playerscore_js, cpuscore_js);
+            }
+            else if (playRound(button.id, tmp_cpu) == 'p2'){
+                cpuscore_js += 1;
+                cpuscore.textContent = `CPU: ${cpuscore_js}`;
+                result.textContent = `CPU wins, ${button.id} loses to ${tmp_cpu}`;
+                checkWinner(playerscore_js, cpuscore_js);
+            }
+            else {
+                result.textContent = "Tie";
+            }
         }
-        else if (playRound(button.id, tmp_cpu) == 'p2'){
-            cpuscore_js += 1;
-            cpuscore.textContent = `CPU: ${cpuscore_js}`;
-            result.textContent = `CPU wins, ${button.id} loses to ${tmp_cpu}`;
-        }
-        else {
-            result.textContent = "Tie";
-        }
+        
     });
 });
